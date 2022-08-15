df666e4c4bfc61ab279b7a5242747c35ecfb877e
Twentyquid <negativeradiations@gmail.com>
2022-08-15 10:12:08 +0530

    added data fetching from the api

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import { MdDeleteOutline, MdFavoriteBorder } from "react-icons/md";
-import defaultImage2 from "../images/default2.png";
+// import defaultImage2 from "../images/default2.png";
 
-function Feed() {
+function Feed(props) {
   return (
     <article className="border-2 border-slate-600 rounded-xl flex flex-col justify-between p-8 mb-8 bg-white">
-      <h3 className="text-lg font-semibold">HIKING AT THE MOUNTAINS</h3>
-      <p className="mt-2 text-slate-500 text-xs">#hiking #mountains</p>
-      <div className="mt-4 ">
-        <img className="ml-auto mr-auto" src={defaultImage2} alt="" />
-      </div>
-      <p className="mt-6 text-sm">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At architecto
-        explicabo ipsa voluptatibus in odit expedita sequi soluta cumque
-        corporis.
+      <h3 className="text-lg font-semibold">{props.title.toUpperCase()}</h3>
+      <p className="mt-2 text-slate-500 text-xs">
+        {props.tags.map((item, i) => {
+          return <span key={i}>{`#${item}`}</span>;
+        })}
       </p>
+      <div className="mt-4 ">
+        <img className="ml-auto mr-auto" src={props.imageFile} alt="" />
+      </div>
+      <p className="mt-6 text-sm">{props.description}</p>
       <div className="mt-6 flex w-full justify-between">
         <button className="rounded-lg border-2 border-blue-600 px-2 py-1 flex items-center">
           <span className="mr-2">
             <MdFavoriteBorder></MdFavoriteBorder>
           </span>
-          Like
+          {props.likeCount}
         </button>
         <button className="px-2 py-1 rounded-lg bg-red-500 text-white flex items-center ml-2">
           <span className="mr-2">
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Feed from "./Feed";
 // import { useEffect, useState } from "react";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 // import { getPosts } from "../reducers/postSlice";
 
 function Home() {
-  // const feed = useSelector((state) => state.posts);
+  const feed = useSelector((state) => state.posts);
   return (
     <section className="h-screen bg-home overflow-y-scroll py-16  px-3">
-      <Feed></Feed>
-      <Feed></Feed>
+      {feed.map((item) => {
+        return <Feed key={item._id} {...item} />;
+      })}
     </section>
   );
 }
